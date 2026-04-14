c85269437178da93f619427265c975733a000e19
victoriacfigueiredo <victoriafigueiredo62@gmail.com>
2026-04-14 00:27:09 -0300

    front and back implementation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import produtos
 
 app = FastAPI(
     title="Sistema de Compras Online",
@@ -6,6 +8,15 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(produtos.router)
 
 @app.get("/", tags=["Health"])
 def health_check():
